486364cc5c804d1c042e2ae1dbf154f29eaa7b61
Guilherme <guilhermecs_30@yahoo.com.br>
2025-01-09 22:16:36 -0300

    Aula estrutura for

---
+++ curso_programacao/src/modulo_repeticao/Aula03.java
@@ -0,0 +1,25 @@
+package modulo_repeticao;
+
+import java.util.Scanner;
+
+public class Aula03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n  = sc.nextInt();
+		int soma = 0;
+
+		for(int i=0; i<n; i++ ) {
+			int x = sc.nextInt();
+			soma += x;
+		}
+		
+		System.out.println(soma);
+		
+		sc.close();
+	}
+
+}
---
+++ curso_programacao/src/modulo_repeticao/Aula04.java
@@ -0,0 +1,18 @@
+package modulo_repeticao;
+
+public class Aula04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (int i=0; i<5; i++) {
+			System.out.println("Valor de i: " + i);
+		}
+		
+		for (int i=4; i>=0; i--) {
+			System.out.println("Valor de i: " + i);
+		}
+
+	}
+
+}
