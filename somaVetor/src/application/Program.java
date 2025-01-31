516140c7ef3c6c91daf61cb5d3f181f00124689c
Guilherme <guilhermecs_30@yahoo.com.br>
2025-01-30 22:34:33 -0300

    Exercicios array1

---
+++ somaVetor/src/application/Program.java
@@ -0,0 +1,41 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Quantos numeros voce vai digitar? ");
+		int n = sc.nextInt();
+		sc.nextLine();
+		double[] vect = new double[n];
+		
+		for(int i = 0; i < vect.length; i++ ) {
+			System.out.print("Digite um numero: ");
+			vect[i] = sc.nextDouble();
+		}
+		
+		System.out.println("Valores");
+		for(int i = 0; i <n; i++) {
+			System.out.print(vect[i] +" " );
+		}
+		
+		double sum =0;
+		
+		for(int i =0; i<vect.length; i++) {
+			sum += vect[i];
+		}
+		
+		double avg = sum / vect.length;
+		System.out.println();
+		System.out.println("SOMA = " + sum);
+		System.out.println("MEDIA = " + avg);
+
+	}
+
+}
